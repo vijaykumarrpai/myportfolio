db0dd6ffe3eced4f722280721f88b92f811d25b0
vijaykumarrpai <vijaykumarrpai@gmail.com>
2020-08-27 17:30:11 +0530

    Updates

---
+++ precache-manifest.5f7ca680611b721e5a797b5c9c4170bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f31d2500bf5f8095ba5e82695bda3a2",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "9d21317669842f02297e",
+    "url": "/myportfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b20f364dd8d2b2840710",
+    "url": "/myportfolio/static/js/2.a73d22a9.chunk.js"
+  },
+  {
+    "revision": "e980a7f3cee8e03adfb625e54adfa237",
+    "url": "/myportfolio/static/js/2.a73d22a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d21317669842f02297e",
+    "url": "/myportfolio/static/js/main.43ac1460.chunk.js"
+  },
+  {
+    "revision": "832c5c04d07544c3a990",
+    "url": "/myportfolio/static/js/runtime-main.7aebf351.js"
+  }
+]);
