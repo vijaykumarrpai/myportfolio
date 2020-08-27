3267035b1835f45f09497414e2906d5e1f104664
vijaykumarrpai <vijaykumarrpai@gmail.com>
2020-08-27 17:25:31 +0530

    final commit

---
+++ src/Components/Accolades.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+
+class Accolades extends Component {
+  render() {
+
+    if(this.props.data){
+      var accolades = this.props.data.accolades.map(function(accolades){
+        return  <li key={accolades.user}>
+            <blockquote>
+               <p>{accolades.text}</p>
+               <cite>{accolades.user}</cite>
+            </blockquote>
+         </li>
+      })
+    }
+
+    return (
+      <section id="accolades">
+      <div className="text-container">
+         <div className="row">
+
+            <div className="two columns header-col">
+               <h1><span>Accolades</span></h1>
+            </div>
+
+            <div className="ten columns flex-container">
+                  <ul className="slides">
+                      {accolades}
+                  </ul>
+               </div>
+            </div>
+         </div>
+   </section>
+    );
+  }
+}
+
+export default Accolades;
