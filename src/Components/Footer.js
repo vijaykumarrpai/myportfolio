92d881a75eaa3bde9d700ec608d4cd25a2ad1c07
Vijaykumar R Pai <vijaykumarrpai@Vijaykumars-MacBook-Air.local>
2024-11-23 13:46:13 +0530

    Copyright year updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2021 Vijaykumar R Pai</li>
+              <li>&copy; Copyright 2024 Vijaykumar R Pai</li>
            </ul>
 
         </div>
